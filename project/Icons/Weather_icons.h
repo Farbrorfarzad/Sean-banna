559702c05a59f14a833166e44d2e8b66a40ec100
Farid <90461105+Farbrorfarzad@users.noreply.github.com>
2025-12-14 19:39:39 +0100

    Add files via upload

--- project/Icons/Weather_icons.h
+++ project/Icons/Weather_icons.h
@@ -1,1 +1,38 @@
+#pragma once
+#include <lvgl.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+extern const lv_img_dsc_t ClearSky;
+extern const lv_img_dsc_t NearlyClearSky;
+extern const lv_img_dsc_t VariableCloudiness;
+extern const lv_img_dsc_t HalfClearSky;
+extern const lv_img_dsc_t CloudySky;
+extern const lv_img_dsc_t Overcast;
+extern const lv_img_dsc_t Fog;
+extern const lv_img_dsc_t LightRainShower;
+extern const lv_img_dsc_t ModerateRainShowers;
+extern const lv_img_dsc_t HeavyRainShowers;
+extern const lv_img_dsc_t Thunderstorm;
+extern const lv_img_dsc_t LightSleetShower;
+extern const lv_img_dsc_t ModerateSleetShowers;
+extern const lv_img_dsc_t HeavySleetShowers;
+extern const lv_img_dsc_t LightSnowShowers;
+extern const lv_img_dsc_t ModerateSnowShowers;
+extern const lv_img_dsc_t HeavySnowShowers;
+extern const lv_img_dsc_t LightRain;
+extern const lv_img_dsc_t ModerateRain;
+extern const lv_img_dsc_t HeavyRain;
+extern const lv_img_dsc_t Thunder;
+extern const lv_img_dsc_t LightSleet;
+extern const lv_img_dsc_t ModerateSleet;
+extern const lv_img_dsc_t HeavySleet;
+extern const lv_img_dsc_t LightSnowfall;
+extern const lv_img_dsc_t ModerateSnowfall;
+extern const lv_img_dsc_t HeavySnowfall;
+#ifdef __cplusplus
+}
+#endif
